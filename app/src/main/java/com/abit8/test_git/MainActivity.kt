256d0e8b127ef04a092de822b05b69dbca4a3447
AhmadMajali <amajali392@gmail.com>
2023-04-27 17:07:39 +0600

    Ahmad Majali:commit1, added 4 screens, 3 onboards, themes ,no status bar, chaned icons in edit text

--- app/src/main/java/com/abit8/test_git/MainActivity.kt
+++ app/src/main/java/com/abit8/test_git/MainActivity.kt
@@ -2,9 +2,14 @@ package com.abit8.test_git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.on_boarding1_fragment)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
     }
 }
